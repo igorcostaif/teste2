f756210a238af7f30c87ff49eea156aea27b9fcb
Igor de Oliveira Costa <igorcosta@ifbapa.intra>
2023-03-15 11:41:28 -0300

    Versao teste

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste do Git");
+		
+		System.out.println("Teste do GIT AQUI");
 	}
 
 }
